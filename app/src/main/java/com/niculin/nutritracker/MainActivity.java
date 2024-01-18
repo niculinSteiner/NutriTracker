d94d388b65d4e103045686a81e791d00f8728f0e
Niculin Steiner <niculin.steiner@ergon.ch>
2024-01-18 11:05:11 +0100

    implement add calories and reset calories functions

--- app/src/main/java/com/niculin/nutritracker/MainActivity.java
+++ app/src/main/java/com/niculin/nutritracker/MainActivity.java
@@ -1,8 +1,13 @@
 package com.niculin.nutritracker;
 
+import static com.niculin.nutritracker.TrackActivity.START_GOAL_KEY;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Button saveButton = (Button)findViewById(R.id.saveGoalButton);
+        saveButton.setOnClickListener(v -> {
+            EditText goal = (EditText) findViewById(R.id.startGoalField);
+            Intent intentToTrackActivity = new Intent(this, TrackActivity.class);
+            intentToTrackActivity.putExtra(START_GOAL_KEY, goal.getText().toString());
+            this.startActivity(intentToTrackActivity);
+        });
     }
 }
--- app/src/main/java/com/niculin/nutritracker/TrackActivity.java
+++ app/src/main/java/com/niculin/nutritracker/TrackActivity.java
@@ -2,13 +2,58 @@ package com.niculin.nutritracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class TrackActivity extends AppCompatActivity {
+
+    public static final String START_GOAL_KEY = "startGoal";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_track);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setGoal();
+        setActionListenerToAddCalories();
+        setActionListenerToResetCalories();
+    }
+
+    private void setActionListenerToResetCalories() {
+        Button resetCaloriesButton = (Button) findViewById(R.id.resetCaloriesButton);
+        resetCaloriesButton.setOnClickListener(v -> {
+            TextView caloriesSoFar = findViewById(R.id.alreadyConsumedTextView);
+            caloriesSoFar.setText("0");
+        });
+    }
+
+    private void setActionListenerToAddCalories() {
+        Button addCaloriesButton = findViewById(R.id.addCaloriesButton);
+        addCaloriesButton.setOnClickListener(v -> {
+            EditText caloriesToAdd = (EditText) findViewById(R.id.addCaloriesField);
+            int caloriesToAddAsInt = Integer.parseInt(caloriesToAdd.getText().toString());
+            TextView caloriesScore = (TextView) findViewById(R.id.alreadyConsumedTextView);
+            if (caloriesScore.getText().toString().isEmpty()){
+                caloriesScore.setText(caloriesToAdd.getText());
+            } else {
+                int caloriesSoFar = Integer.parseInt(caloriesScore.getText().toString());
+                caloriesScore.setText(String.valueOf(caloriesSoFar + caloriesToAddAsInt));
+            }
+        });
+    }
+
+
+    protected void setGoal() {
+        Intent intent = getIntent();
+        int goal = Integer.parseInt(intent.getExtras().getString(START_GOAL_KEY));
+        TextView view = findViewById(R.id.goalTextView);
+        view.setText(String.valueOf(goal));
     }
 }
